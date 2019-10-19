f4adaeb9baabe4b66d5eddb1d3ff2c1cf8118299
Administrator <760704065@qq.com>
2019-10-19 19:35:13 +0800

    页面路由配置，elementui安装，axios安装

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ceshi from '@/components/ceshi'
+import list from '@/components/list'
+import details from '@/components/details'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path: '/', name: 'HelloWorld', component: HelloWorld},
+    {path: '/ceshi', name: 'ceshi', component: ceshi},
+    {path: '/list', name: 'list', component: list},
+    {path: '/details', name: 'details', component: details},
+
   ]
 })
